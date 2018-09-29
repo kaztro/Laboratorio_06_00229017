ec49bad76f7f0adfafed24b7fa9ec064d799ec4e
kaztro <00229017@uca.edu.sv>
2018-09-29 01:28:00 -0600

    finished?

--- Ejercicio_12.js
+++ Ejercicio_12.js
@@ -1,17 +1,21 @@
-let menu = function() {
+/*let menu = function() {
     console.log('1.Ingresar Persona');
     console.log('2.Buscar Persona');
     console.log('3.Modificar Persona');
     console.log('4.Eliminar Persona');
     console.log('5.Mostrar todas las Personas');
     console.log('6.Salir'); 
-}
+}*/
 
 let validarMenu = function() {
-    menu();
+    console.log('1.Ingresar Persona');
+    console.log('2.Buscar Persona');
+    console.log('3.Modificar Persona');
+    console.log('4.Eliminar Persona');
+    console.log('5.Mostrar todas las Personas');
+    console.log('6.Salir');  
     op = prompt('Ingresa una de las opciones:');
-    while(op != 1 || op != 2 || op != 3 || op != 4 || op != 5 || op != 6) {
-        menu();
+    while(op == 1 || op == 2 || op == 3 || op == 4 || op == 5 || op == 6) {
         op = prompt('Ingresa una de las opciones:');
     }
     E12(op);
