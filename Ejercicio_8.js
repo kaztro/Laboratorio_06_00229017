4d959e0e83012f3129d83cd161e3834387a74b5f
kaztro <00229017@uca.edu.sv>
2018-09-28 22:22:11 -0600

    Hasta el 8 completo :U

--- Ejercicio_8.js
+++ Ejercicio_8.js
@@ -1,20 +1,41 @@
-let creaMatriz3x3 = function() {
-    var matriz = new Array(3);
-    for (let k = 0; k < 3; k++) matriz[k] = new Array(3);
-    return matriz    
+//Crea la estructura de la matriz
+let creaMatriznxn = function(orden) {
+    var matriz = new Array(orden);
+    for (let k = 0; k < orden; k++) matriz[k] = new Array(orden);
+    return matriz;    
 }
 
-let llenarMatriz3x3 = function(matriz) {
-    for (let i = 0; i < 3; i++) {
-        for(let j = 0; j < 3; j++) {
+//Llena la matriz con numeros del 0 al 9 al azar
+let llenarMatriznxn = function(matriz, orden) {
+    for (let i = 0; i < orden; i++) {
+        for(let j = 0; j < orden; j++) {
             matriz[i][j] = Math.floor(Math.random() * 10);  
         }
     }
-    return llenarMatriz3x3;
+    return matriz;
 }
 
-let E8 = function(a, b) {
-    
+let E8 = function(a, b, ordenA, ordenB) {
+    if (ordenA != ordenB) {
+        return 'Esto no puede operarse...'
+    }
+    var orden = ordenA;
+        c = creaMatriznxn(orden);
+        for (let i = 0; i < orden; i++) {
+            for(let j = 0; j < orden; j++) {
+               c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+    return c;
 }
-
-E8(llenarMatriz3x3(creaMatriz3x3()), llenarMatriz3x3(creaMatriz3x3()));
+//Primera matriz 'A'; orden: 4:
+ordenA = 4;
+A = llenarMatriznxn(creaMatriznxn(ordenA), ordenA);
+console.log(A);
+//Primera matriz 'B'; orden: 4:
+ordenB = 4;
+B = llenarMatriznxn(creaMatriznxn(ordenB), ordenB);
+console.log(B);
+//Suma de funciones:
+console.log('A+B=C; C= ');
+console.log(E8(A, B, ordenA, ordenB));
